0339fac9edcc17eac648a793480d8093c60558b0
jpcadinelli <jpcadinelli@gmail.com>
2024-12-09 19:57:33 -0300

    feat: adição do visualizar venda

--- api/controller/venda/venda.go
+++ api/controller/venda/venda.go
@@ -9,6 +9,7 @@ import (
 	"api_pattern_go/api/repository"
 	"api_pattern_go/api/service"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -36,6 +37,34 @@ func Criar(ginctx *gin.Context) {
 	}
 
 	ginctx.JSON(http.StatusCreated, middleware.NewResponseBridge(nil, venda))
+}
+
+func Visualizar(ginctx *gin.Context) {
+	usuarioLogado, err := service.GetUsuarioLogado(ginctx)
+	if err != nil {
+		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	if !service.VerificaPermissaoUsuario(*usuarioLogado, enum.PermissaoVendaVisualizar) {
+		ginctx.JSON(http.StatusUnauthorized, middleware.NewResponseBridge(erros.ErrUsuarioNaoTemPermissao, nil))
+		return
+	}
+
+	idStr := ginctx.Param("id")
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	venda, err := repository.NewVendaRepository(dbConetion.DB).FindById(id, "Produtos")
+	if err != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	ginctx.JSON(http.StatusOK, middleware.NewResponseBridge(nil, venda))
 }
 
 func Listar(ginctx *gin.Context) {
--- api/routes/venda.go
+++ api/routes/venda.go
@@ -7,7 +7,7 @@ import (
 
 func vendaRoutes(r *gin.RouterGroup) {
 	r.POST(route, venda.Criar)
-	//r.GET(routeId, venda.Visualizar)
+	r.GET(routeId, venda.Visualizar)
 	r.POST(routeFiltro, venda.Listar)
 	//r.GET(routeDropdown, venda.Dropdown)
 	//r.PUT(route, venda.Atualizar)
