307df5cd38e9eb9396aba0da77f5f3c6a37421dc
jpcadinelli <jpcadinelli@gmail.com>
2024-12-09 19:16:02 -0300

    feat: adição do endpoint que procura no estoque por id de produto

--- api/repository/estoque.go
+++ api/repository/estoque.go
@@ -9,6 +9,7 @@ import (
 
 type EstoqueRepository interface {
 	FindById(id uuid.UUID, preloads ...string) (*models.Estoque, error)
+	FindByIdProduto(idProduto uuid.UUID, preloads ...string) (*models.Estoque, error)
 	Create(estoque *models.Estoque) error
 	Update(estoque *models.Estoque, updateItems map[string]interface{}) (*models.Estoque, error)
 }
@@ -40,6 +41,27 @@ func (r *estoqueRepositoryImpl) FindById(id uuid.UUID, preloads ...string) (*mod
 	}
 
 	return &estoque, nil
+}
+
+func (r *estoqueRepositoryImpl) FindByIdProduto(idProduto uuid.UUID, preloads ...string) (*models.Estoque, error) {
+	var estoque *models.Estoque
+
+	tx := r.db
+	if len(preloads) > 0 {
+		for _, preload := range preloads {
+			tx = tx.Preload(preload)
+		}
+	}
+
+	tx = tx.Find(&estoque, "id_produto = ? AND quantidade > 0", idProduto)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return nil, nil
+	}
+
+	return estoque, nil
 }
 
 func (r *estoqueRepositoryImpl) Create(estoque *models.Estoque) error {
